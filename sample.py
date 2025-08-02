a7781c311a10bc891eb8f901f7babf0899618de6
Vikas <teachvikasedu@gmail.com>
2025-08-02 07:40:14 +0530

    git revert demo
    This reverts commit 7f7859e9037816eb28138be88d3eafc95e3f1f99.

--- sample.py
+++ sample.py
@@ -1,10 +1,9 @@
 a = 512
-b = 659
-d = 658
+b = 658
 c = 86
 
-sum = a + b + c + d
-print  "the sum of four numbers is = " sum 
+sum = a + b + c
+print  "the sum of three numbers is = " sum 
 print "changes done on dev branch"
-print " adding a new line for git revert"
+
 
